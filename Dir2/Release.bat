1473a8f79f9ab4fb1df6b1fafe76f97cef9e8296
stackprobe <stackprobe@gmail.com>
2016-03-05 06:16:29 +0900

    b

--- Dir2/Release.bat
+++ Dir2/Release.bat
@@ -1,10 +1,10 @@
 C:\Factory\Tools\RDMD.exe /RC out
 
 COPY Dir2\Dir2\bin\Release\Dir2.exe out
-COPY Dir2Tools\Dir2Tools\bin\Release\Dir2Tools.exe out
-COPY AddFilePart\AddFilePart\bin\Release\AddFilePart.exe out
-COPY GetFilePart\GetFilePart\bin\Release\GetFilePart.exe out
-COPY SetFileTime\SetFileTime\bin\Release\SetFileTime.exe out
+COPY Dir2\Dir2Tools\bin\Release\Dir2Tools.exe out
+COPY Dir2\AddFilePart\bin\Release\AddFilePart.exe out
+COPY Dir2\GetFilePart\bin\Release\GetFilePart.exe out
+COPY Dir2\SetFileTime\bin\Release\SetFileTime.exe out
 C:\Factory\Tools\xcp.exe doc out
 
 C:\Factory\Tools\zcp.exe out C:\app\Kit\Dir2
