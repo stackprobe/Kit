fde371d0df9e4d2efee23a1640156502ba952cb7
stackprobe <stackprobe@gmail.com>
2016-10-19 03:30:01 +0900

    a

---
+++ FilingCase3/FilingCase3/FilingCase3/MainWin.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Charlotte
+{
+	partial class MainWin
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWin));
+			this.TaskTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.TTIMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.MainTimer = new System.Windows.Forms.Timer(this.components);
+			this.TTIMenu.SuspendLayout();
+			this.SuspendLayout();
+			//
+			// TaskTrayIcon
+			//
+			this.TaskTrayIcon.ContextMenuStrip = this.TTIMenu;
+			this.TaskTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TaskTrayIcon.Icon")));
+			this.TaskTrayIcon.Text = "FilingCase3";
+			//
+			// TTIMenu
+			//
+			this.TTIMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了XToolStripMenuItem});
+			this.TTIMenu.Name = "TTIMenu";
+			this.TTIMenu.Size = new System.Drawing.Size(119, 26);
+			//
+			// 終了XToolStripMenuItem
+			//
+			this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+			this.終了XToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+			this.終了XToolStripMenuItem.Text = "終了(&X)";
+			this.終了XToolStripMenuItem.Click += new System.EventHandler(this.終了XToolStripMenuItem_Click);
+			//
+			// MainTimer
+			//
+			this.MainTimer.Enabled = true;
+			this.MainTimer.Interval = 300;
+			this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+			//
+			// MainWin
+			//
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Location = new System.Drawing.Point(-400, -400);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainWin";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "FilingCase3_FourRoses";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWin_FormClosing);
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWin_FormClosed);
+			this.Load += new System.EventHandler(this.MainWin_Load);
+			this.Shown += new System.EventHandler(this.MainWin_Shown);
+			this.TTIMenu.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon TaskTrayIcon;
+		private System.Windows.Forms.ContextMenuStrip TTIMenu;
+		private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+		private System.Windows.Forms.Timer MainTimer;
+	}
+}
+
---
+++ FilingCase3/FilingCase3/FilingCase3/MainWin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Permissions;
+
+namespace Charlotte
+{
+	public partial class MainWin : Form
+	{
+		#region ALT_F4 抑止
+
+		[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+		protected override void WndProc(ref Message m)
+		{
+			const int WM_SYSCOMMAND = 0x112;
+			const long SC_CLOSE = 0xF060L;
+
+			if (m.Msg == WM_SYSCOMMAND && (m.WParam.ToInt64() & 0xFFF0L) == SC_CLOSE)
+				return;
+
+			base.WndProc(ref m);
+		}
+
+		#endregion
+
+		public MainWin()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWin_Load(object sender, EventArgs e)
+		{
+			// noop
+		}
+
+		private void MainWin_Shown(object sender, EventArgs e)
+		{
+			this.Visible = false;
+			this.TaskTrayIcon.Visible = true;
+			this.MT_Enabled = true;
+		}
+
+		private void MainWin_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			// noop
+		}
+
+		private void MainWin_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			this.MT_Enabled = false;
+			this.TaskTrayIcon.Visible = false;
+		}
+
+		private void 終了XToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private bool MT_Enabled;
+		private bool MT_Busy;
+		private long MT_Count;
+
+		private void MainTimer_Tick(object sender, EventArgs e)
+		{
+			if (this.MT_Enabled == false || this.MT_Busy)
+				return;
+
+			this.MT_Busy = true;
+
+			try
+			{
+				// TODO
+			}
+			finally
+			{
+				this.MT_Busy = false;
+				this.MT_Count++;
+			}
+		}
+	}
+}
