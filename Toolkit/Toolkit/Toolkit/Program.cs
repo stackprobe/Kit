0ae3fc3c0ef31793859f3750b1960ce3b7bdb915
stackprobe <stackprobes@gmail.com>
2020-08-21 11:17:27 +0900

    Polydeuces

--- Toolkit/Toolkit/Toolkit/Program.cs
+++ Toolkit/Toolkit/Toolkit/Program.cs
@@ -54,7 +54,10 @@ namespace Toolkit
 					using (Bitmap bmp = new Bitmap(img))
 					using (Graphics g = Graphics.FromImage(bmp))
 					{
-						g.FillRectangle(Brushes.DarkRed, 0, 0, bmp.Width, bmp.Height); // 単色
+						//Color DARK_RED = Color.DarkRed;
+						Color BORDEAUX = Color.FromArgb(96, 15, 24);
+
+						g.FillRectangle(new SolidBrush(BORDEAUX), 0, 0, bmp.Width, bmp.Height); // 単色
 
 						bmp.Save(wFile);
 					}
